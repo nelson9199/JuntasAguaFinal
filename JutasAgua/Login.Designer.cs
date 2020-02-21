40bbdbb0bfaa5e89fdbed9813037573e090cfbe8
Nelson Marro <nelsonmarro99@outlook.com>
2020-02-21 09:50:33 -0500

    Agregar archivos de proyecto.

---
+++ JutasAgua/Login.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace JutasAgua
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.SuspendLayout();
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(12, 41);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(81, 25);
+            this.bunifuCustomLabel1.TabIndex = 0;
+            this.bunifuCustomLabel1.Text = "Usuario:";
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(12, 92);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(81, 25);
+            this.bunifuCustomLabel2.TabIndex = 0;
+            this.bunifuCustomLabel2.Text = "Usuario:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(381, 187);
+            this.Controls.Add(this.bunifuCustomLabel2);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+    }
+}
+
---
+++ Modelos/repositoryAgua.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Data.DataSet;
+
+namespace Modelos
+{
+    public class RepositoryAgua
+    {
+        //Tabla usuario
+        public List<usuario> hacerLogin(string user, string clave)
+        {
+            using(var contexto = new juntas_aguaEntities())
+            {
+                var usuario =
+                    from c in contexto.usuario
+                    where c.nombre == user && c.clave == clave
+                    select c;
+
+                return usuario.ToList();
+            }
+        }
+
+        //Tabla empresa
+        public List<empresa> obtenerEmpresas()
+        {
+            using (var contexto = new juntas_aguaEntities())
+            {
+                var empresa =
+                    from e in contexto.empresa
+                    select e; 
+
+                return empresa.ToList();
+            }
+        }
+
+        //Tabla tarifa
+        public List<tarifa>OtenerTarifas()
+        {
+            using (var contexto = new juntas_aguaEntities())
+            {
+                var tarifas =
+                    from t in contexto.tarifa
+                    select t;
+
+                return tarifas.ToList();
+            }
+        }
+
+        //Tabla cliente
+        public List<cliente> OtenerClientes()
+        {
+            using (var contexto = new juntas_aguaEntities())
+            {
+                var clientes =
+                    from c in contexto.cliente
+                    select c;
+
+                return clientes.ToList();
+            }
+        }
+    }
+}
